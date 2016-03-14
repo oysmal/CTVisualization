e0c0a4eed880f6983ae65d5b21328f949e0af0d0
Øystein Malt <ystein Malt>
2016-03-14 19:38:14 +0100

    Fixed some 404 error handling stuff

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // // error handlers
 // //
