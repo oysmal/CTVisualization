31aa0f6c472c807e82045ab0854f28f30fc346ba
Megan <megan_4@live.ca>
2016-03-14 23:54:55 +0100

    latest updates

--- testing.js
+++ testing.js
@@ -1,10 +1,21 @@
 var fs = require('fs');
 
-// Synchronous read
-var data = fs.readFileSync('hand.dat');
-var x = data.readUIntBE(0,1);
-var y = data.readUIntBE(2,1);
-var z = data.readUIntBE(3,1);
-console.log("values: " + x+" "+y+" " +z);
+// // Synchronous read
+var data = fs.readFileSync('hand.dat','hex');
+
+var byteStringX = data.substr(0, 2);
+var byteStringY = data.substr(4, 2);
+var byteStringZ = data.substr(8,4);
+var x= parseInt("0x" + byteStringX);
+var y= parseInt("0x" + byteStringY);
+var z= parseInt("0xce03");
+console.log("values: "+byteStringX+": "+x+" "+byteStringY+": "+y+" "+byteStringZ+": "+z);
+// var i,j;
+// var results= new Array(z);
+// for(i=0;i<x;i++){
+// 	for(j=0;j<y;j++){
+// //add results to array
+// 	}
+// }
 
 console.log("Program Ended");
